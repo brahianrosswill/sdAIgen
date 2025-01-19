87c339658e4e755643e97f32b9726f7317d214de
anxety-solo <YuukiSanNya@yandex.ru>
2025-01-19 21:23:54 +0900

    BAN! 👽️

--- scripts/ru/downloading-ru.py
+++ scripts/ru/downloading-ru.py
@@ -66,6 +66,7 @@ def setup_venv():
     # Installing dependencies
     install_commands = []
     if ENV_NAME == 'Google Colab':
+        """
         # I agree with the author of the correction here ;3
         for blyat in [
             'sudo ln -sf /usr/bin/python3.10 /usr/local/bin/python',
@@ -74,6 +75,7 @@ def setup_venv():
             'sudo ln -sf /usr/local/lib/python3.11 /usr/local/lib/python3.10'
         ]:
             ipySys(blyat)
+        """
 
         install_commands = ["apt -y install python3.10-venv"]
     else:
@@ -93,11 +95,12 @@ def setup_venv():
     ipySys(f'rm -rf {VENV}/bin/pip* {VENV}/bin/python*')
 
     # Create a virtual environment
+    python_command = 'python3.10' if ENV_NAME == 'Google Colab' else 'python3'    # BAN!
     venv_commands = [
-    #     f'python3.10 -m venv {VENV}',
-        f'python3 -m venv {VENV}',
+    #     f'python3 -m venv {VENV}',
+        f'{python_command} -m venv {VENV}',
         f'{VENV}/bin/python3 -m pip install -U --force-reinstall pip',
-    #     f'{VENV}/bin/python3 -m pip install ipykernel',
+        f'{VENV}/bin/python3 -m pip install ipykernel',
         f'{VENV}/bin/python3 -m pip uninstall -y ngrok pyngrok'
     ]
     if UI in ['Forge', 'ComfyUI']:
