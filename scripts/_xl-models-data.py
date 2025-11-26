dab3e2c902a001dc527763acaec56ba4f2fba400
anxety-solo <YuukiSanNya@yandex.ru>
2025-11-26 11:24:59 +0900

    shits

--- scripts/_xl-models-data.py
+++ scripts/_xl-models-data.py
@@ -1,14 +1,14 @@
 ## MODEL
 
 model_list = {
-    "1. Flanime-IL [Anime] [V2] [XL]": [
-        {'url': "https://civitai.com/api/download/models/2125353", 'name': "FlanimeXL-illustrious_V2.safetensors"}
+    "1. Flanime-IL [Anime] [V2.7] [XL]": [
+        {'url': "https://civitai.com/api/download/models/2437203", 'name': "FlanimeXL-illustrious_V2.7.safetensors"}
     ],
-    "2. Hassaku-XL [Anime] [V3.1] [XL]": [
-        {'url': "https://civitai.com/api/download/models/2262382", 'name': "HassakuXL-illustrious_V3.1.safetensors"}
+    "2. Hassaku-XL [Anime] [V3.2] [XL]": [
+        {'url': "https://civitai.com/api/download/models/2337366", 'name': "HassakuXL-illustrious_V3.2.safetensors"}
     ],
-    "3. Nova-IL [Anime] [V12] [XL]": [
-        {'url': "https://civitai.com/api/download/models/2268916", 'name': "Nova-illustrious_V12.safetensors"}
+    "3. Nova-IL [Anime] [V13] [XL]": [
+        {'url': "https://civitai.com/api/download/models/2392184", 'name': "Nova-illustrious_V13.safetensors"}
     ],
     "4. NoobAI [Anime] [VP-1.0] [XL]": [
         {'url': "https://civitai.com/api/download/models/1190596", 'name': "NoobAI_VP1.safetensors"}
